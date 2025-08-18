ea95e8a34b3ddbb152c34e696e2c736d1a045086
fenndragon <fenndragon@gmail.com>
2025-08-17 21:26:43 -0600

    Update SalvageExpeditionComponent.cs

--- Content.Server/Salvage/Expeditions/SalvageExpeditionComponent.cs
+++ Content.Server/Salvage/Expeditions/SalvageExpeditionComponent.cs
@@ -61,4 +61,17 @@ public sealed partial class SalvageExpeditionComponent : SharedSalvageExpedition
     // [DataField]
     // public ResolvedSoundSpecifier SelectedSong;
     // End Frontier: moved to Shared
+    // Frontier: expedition difficulty and rewards
+    /// <summary>
+    /// The difficulty this mission had or, in the future, was selected.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("difficulty")]
+    public DifficultyRating Difficulty;
+
+    /// <summary>
+    /// List of items to order on mission completion
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("rewards", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>))]    [ViewVariables(VVAccess.ReadWrite), DataField("rewards", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>))]
+    public List<string> Rewards = default!;    public List<string> Rewards = default!;
+    // End Frontier: expedition difficulty and rewards    // End Frontier: expedition difficulty and rewards
 }
